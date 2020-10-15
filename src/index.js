26a14ad59408f69d17d75e6f204700bd6f844c2c
JangJua <jua0610@naver.com>
2020-10-15 16:49:01 +0900

    Set up Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
